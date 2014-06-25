eeda1620a75c3a6196aea7987fd5987ae623aa26
delucks <jamie.delucks@gmail.com>
2014-06-25 12:45:17 -0400

    Ansible software installation started, updating setup.sh to be with the times

--- setup.sh
+++ setup.sh
@@ -1,57 +1,40 @@
 #!/bin/bash
-
-# File to add in all the necessary stuffs for my dotfiles
-# Dat automation
-
 # Author: delucks
 
 set -e
 
-function wallpapers {
-	echo ":: Downloading wallpapers pack..."
-	if [ $(which wget) ]; then
-		wget -nc -c -O /tmp/wall.tar.gz http://www.jamesluck.com/raw/wall.tar.gz
-	else
-		curl http://www.jamesluck.com/raw/wall.tar.gz -o /tmp/wall.tar.gz
-	fi
-	echo ":: Extracting wallpapers pack to ~/dev/wp & ~/wallpapers..."
-	cd
-	tar xvf /tmp/wall.tar.gz
-	echo ":: Done."
-}
-
 function linkOrMove {
-	echo ":: Processing ~/dotfiles/$1 to ~/.$1"
-	ln -s ~/dotfiles/$1 ~/.$1 > /dev/null 2>&1
+	echo "Processing ~/dotfiles/$1 to ~/.$1"
+	ln -s ~/dotfiles/$1 ~/$1
 	if [ $? -ne 0 ]; then
-		if [ -h ~/.$1 ]; then
-			echo ":: WARNING :: A symlink already exists for $1. Check this manually."
-			return 1
+		if [ -h ~/$1 ]; then
+			echo "WARNING A symlink already exists for $1. Unlinking it."
+			unlink ~/$1
+			ln -s ~/dotfiles/$1 ~/$1
+			[ $? -eq 0 ] && echo "~/$1 linked successfully."
 		else
-			echo ":: ~/.$1 exists. Copying it to ~/.$1.bak"
-			cp ~/.$1{,.bak}
-			echo ":: Now removing the old ~/.$1"
-			rm ~/.$1
-			echo ":: Now linking ~/.$1"
-			ln -s ~/dotfiles/$1 ~/.$1
-			[ $? -eq 0 ] && echo ":: ~/.$1 linked successfully."
+			echo "~/$1 exists. Copying it to ~/$1.bak"
+			cp ~/$1{,.bak}
+			echo "Now removing the old ~/$1"
+			rm ~/$1
+			echo "Now linking ~/$1"
+			ln -s ~/dotfiles/$1 ~/$1
+			[ $? -eq 0 ] && echo "~/$1 linked successfully."
 		fi
 	else
-		echo ":: ~/.$1 linked successfully."
+		echo "~/$1 linked successfully."
 	fi
-	echo ""
 }
 
 function usage {
 	echo "usage: setup.sh [option]"
 	echo
 	echo "Options:"
-	echo "    -i, --incremental - Not yet implemented, don't use"
 	echo "    -l, --headless    - Install everything not X-specific"
-	echo "    -x, --xorg        - Install everything"
+	echo "    -a, --all         - Install everything"
+	echo "    -s, --software    - Install everything, and try to get the correct software installed (requires sudo)"
 	echo "    -h, --help        - Show this help message"
 	echo "    -v, --version     - Show a quick version summary"
-	echo "    -w, --wallpaper   - Bootstrap my current wallpaper & colors"
 	exit
 }
 
@@ -59,38 +42,23 @@ function version {
 	echo "setup.sh by delucks@github.com"
 	echo
 	echo "Contact:      jluck@udel.edu"
-	echo "Version:      0.5a"
+	echo "Version:      0.6"
 	echo
 	echo "Thanks for using my dotfiles!"
 	exit
 }
 
-homeDotfiles=("Xcolors" "Xmodmap" "Xresources" "abcde.conf" "bashrc" "compton.conf" "dmenurc" "i3" "i3status.conf" "irssi" "redshiftgrc" "tmux.conf" "vim" "vimrc" "xinitrc" "xmobarrc" "zshrc")
-
-headlessHomeDotfiles=("bashrc" "irssi" "tmux.conf" "vim" "vimrc" "zshrc")
-
 case "$1" in
 	'-l'|'--headless')
-		for i in "${headlessHomeDotfiles[@]}"
-		do
-			linkOrMove $i
-		done
+		dotfiles=(".bashrc" ".irssi" ".tmux.conf" ".vim" ".vimrc" ".zshrc" ".abcde.conf" ".gitconfig" ".ncmpcpp")
 		;;
-	'-x'|'--xorg')
-		for i in "${homeDotfiles[@]}"
-		do
-			linkOrMove $i
-		done
-		;;
-	'-i'|'--incremental')
-		echo "Not yet implemented! Will have incremental git links later on. Sorry :)"
-		exit 0
+	'-a'|'--all')
+		dotfiles=(".bashrc" ".irssi" ".tmux.conf" ".vim" ".vimrc" ".zshrc" ".abcde.conf" ".gitconfig" ".ncmpcpp" ".Xresources" ".xinitrc" ".Xmodmap" ".compton.conf" ".dmenurc" ".i3" ".redshiftgrc" ".xmobarrc")
+		mkdir -p ~/.config
+		cp -R ~/dotfiles/herbstluftwm/ ~/.config/herbstluftwm
 		;;
 	'-h'|'--help')
 		usage
-		;;
-	'-w'|'--wallpapers')
-		wallpapers
 		;;
 	'-v'|'--version')
 		version
@@ -100,3 +68,24 @@ case "$1" in
 		exit 1
 		;;
 esac
+
+echo "Installing delucks/scripts repo"
+git clone https://github.com/delucks/scripts ~/scripts
+echo "Installing oh-my-zsh"
+git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+echo "Installing zsh-syntax-highlighting"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/zsh-syntax-highlighting
+
+for item in "${dotfiles[@]}"
+do
+	linkOrMove $item
+done
+
+cp ~/dotfiles/delucks.zsh-theme ~/.oh-my-zsh/themes/
+mkdir -p ~/.config
+cp -R ~/dotfiles/ranger/ ~/.config/ranger
+
+echo "Installing Vundle"
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+echo "Installing vim plugins managed by Vundle"
+vim +PluginInstall! +qall
