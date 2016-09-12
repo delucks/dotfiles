4ab7db8f06284d21250d678ec6e0c03f42ac92c7
delucks <jamie.delucks@gmail.com>
2016-09-11 22:22:09 -0700

    Removed hardcoded references to scripts

--- configure-x11.sh
+++ configure-x11.sh
@@ -3,8 +3,11 @@
 # sets up the few variables needed for the dynamic x11 setup to work.
 USER=$(whoami)
 DEFAULT_SCHEME=x11/.config/colors/zbrah.colors
+SCHEME_DEST=$HOME/.colors
+
 I3_CONFIG=x11/.i3/config
 I3_TEMPLATE=x11/.i3/config-template
+
 XRESOURCES=x11/.Xresources
 XRESOURCES_TEMPLATE=x11/.Xresources-template
 
@@ -12,28 +15,28 @@ source x11/bin/common.sh
 x11::parsecolors "${DEFAULT_SCHEME}"
 
 x11::templatei3() {
-  if [ -f $I3_CONFIG ]; then
-    echo "Cannot clobber existing template"
+  if [ -f "$I3_CONFIG" ]; then
+    echo "Cannot clobber existing templated $I3_CONFIG"
     return 1
   fi
   for i in $(seq 0 17); do
     varname=color$i
     echo "set \$color$i ${!varname}"
-  done | tee $I3_CONFIG
-  echo | tee -a $I3_CONFIG
-  cat $I3_TEMPLATE | tee -a $I3_CONFIG
+  done > "$I3_CONFIG"
+  echo >> "$I3_CONFIG"
+  cat "$I3_TEMPLATE" >> "$I3_CONFIG"
   echo
   echo "i3 successfully templated"
   echo
 }
 
 x11::templateXresources() {
-  if [ -f $XRESOURCES ]; then
-    echo "Cannot clobber existing template"
+  if [ -f "$XRESOURCES" ]; then
+    echo "Cannot clobber existing templated $XRESOURCES"
     return 1
   fi
-  echo "#include \"/home/$USER/.colors\"" | tee $XRESOURCES
-  cat $XRESOURCES_TEMPLATE | tee -a $XRESOURCES
+  echo "#include \"/home/$USER/.colors\"" > "$XRESOURCES"
+  cat "$XRESOURCES_TEMPLATE" >> "$XRESOURCES"
   echo
   echo ".Xresources successfully templated"
   echo
