5e76c25c962eee003282140d69a6be2611f3e1e2
delucks <me@jamesluck.com>
2017-08-09 22:23:19 -0700

    Platform detection for tmux statusline

--- common/.tmux/status.sh
+++ common/.tmux/status.sh
@@ -1,11 +1,48 @@
+#!/usr/bin/env bash
+
+# give me basic system stats for tmux statusline
+
+if [[ "$OSTYPE" == "darwin"* ]]
+then
+  memcmd() {
+    PAGESZ=$(sysctl hw.pagesize | awk '{print $NF}')
+    FREE=$(( $(vm_stat | awk '/free/ {gsub(/\./, "", $3); print $3}') * PAGESZ))
+    TOTAL=$(sysctl hw.memsize | awk '{print $2}')
+    RATIO=$(bc -l <<< "$FREE/$TOTAL*100")
+    printf '%3.1f' $RATIO
+    echo %
+  }
+  loadcmd() {
+    uptime | rev | cut -d' ' -f '1-3' | rev
+  }
+elif [[ "$OSTYPE" == "freebsd"* ]]
+then
+  memcmd() {
+    avail=$(sysctl hw.physmem | awk '{print $2}')
+    used=$(vmstat -H | tail -1 | awk '{print $5}')
+    echo "$((used/avail))%"
+  }
+  loadcmd() {
+    uptime | rev | cut -d' ' -f '1-3' | rev
+  }
+else
+  # assume linux
+  memcmd() {
+    free | awk '/Mem/{PERC=$NF/$2*100.0; printf "%3.1f%\n", PERC}'
+  }
+  loadcmd() {
+    cut -d' ' -f '1-3' /proc/loadavg
+  }
+fi
+
 case $1 in
   load)
-    cut -d\  -f 1-3 /proc/loadavg
+    loadcmd
     ;;
   unix)
     date +%s
     ;;
   memory|mem)
-    free | awk '/Mem/{PERC=$NF/$2*100.0; printf "%3.1f%\n", PERC}'
+    memcmd
     ;;
 esac
