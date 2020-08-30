8d63a3442d22b841aff5e9799caaa6c14ead74c7
Jamie Luck <ping@jamieluck.com>
2020-08-30 13:13:45 -0400

    Monitor swap script supports hidpi

--- x11/bin/swap_monitor.sh
+++ x11/bin/swap_monitor.sh
@@ -1,26 +1,47 @@
 #!/bin/bash -e
 
-# Swaps between the internal and external monitors
+# Swaps between the internal and external monitors.
+# My external monitor is a 4K display which should use scaled DPI, so this script also modifies ~/.Xresources
+
+# A previous version set a custom modeline for the new monitor, here is the procedure should I need it again:
+#RESOLUTION="2560 1080 44"
+#MODELINE=$(cvt $RESOLUTION | cut -f2 -d$'\n')
+#MODEDATA=$(echo "$MODELINE" | cut -f 3- -d' ')
+#MODENAME=$(echo "$MODELINE" | cut -f2 -d' ')
+#if ! xrandr --current | grep -q "$MODENAME"; then
+#  # External monitor doesn't have the correct resolution in this X session yet: create it
+#  echo "Adding mode - $MODENAME $MODEDATA"
+#  xrandr --newmode "$MODENAME" $MODEDATA
+#  xrandr --addmode "$EXTERNAL_DISPLAY" "$MODENAME"
+#fi
+#xrandr --output "$EXTERNAL_DISPLAY" --primary --mode "$MODENAME" --pos 0x0 --rotate normal --output "$INTERNAL_DISPLAY" --off
 
 INTERNAL_DISPLAY="eDP-1"
-EXTERNAL_DISPLAY="HDMI-1"
-RESOLUTION="2560 1080 44"
-MODELINE=$(cvt $RESOLUTION | cut -f2 -d$'\n')
-MODEDATA=$(echo "$MODELINE" | cut -f 3- -d' ')
-MODENAME=$(echo "$MODELINE" | cut -f2 -d' ')
+EXTERNAL_DISPLAY="HDMI-2"
+
+lowdpi() {
+  sed -i 's/^Xft.dpi: 192/Xft.dpi: 96/g' "$HOME/.Xresources"
+  xrdb "$HOME/.Xresources"
+}
+
+hidpi() {
+  sed -i 's/^Xft.dpi: 96/Xft.dpi: 192/g' "$HOME/.Xresources"
+  xrdb "$HOME/.Xresources"
+}
 
 swap_to_internal() {
+  lowdpi
   xrandr --output "$EXTERNAL_DISPLAY" --off --output "$INTERNAL_DISPLAY" --auto
 }
 
 swap_to_external() {
-  if ! xrandr --current | grep -q "$MODENAME"; then
-    # External monitor doesn't have the correct resolution in this X session yet: create it
-    echo "Adding mode - $MODENAME $MODEDATA"
-    xrandr --newmode "$MODENAME" $MODEDATA
-    xrandr --addmode "$EXTERNAL_DISPLAY" "$MODENAME"
-  fi
-  xrandr --output "$EXTERNAL_DISPLAY" --primary --mode "$MODENAME" --pos 0x0 --rotate normal --output "$INTERNAL_DISPLAY" --off
+  hidpi
+  xrandr --output "$EXTERNAL_DISPLAY" --primary --mode 3840x2160 --pos 0x0 --rotate normal --output "$INTERNAL_DISPLAY" --off
+}
+
+right_of_external() {
+  hidpi
+  xrandr --output "$INTERNAL_DISPLAY" --mode 1920x1080 --pos 3840x1080 --rotate normal --output "$EXTERNAL_DISPLAY" --primary --mode 3840x2160 --pos 0x0 --rotate normal
 }
 
 if [[ $# -gt 0 ]]; then
@@ -31,14 +52,9 @@ if [[ $# -gt 0 ]]; then
     '-e'|'--external')
       swap_to_external
       ;;
+    '-r'|'--right')
+      right_of_external
+      ;;
   esac
   exit 0
-fi
-
-if xrandr --current | grep -q "$EXTERNAL_DISPLAY disconnected"; then
-  echo "Swapping to internal display- $EXTERNAL_DISPLAY is disconnected"
-  swap_to_internal
-else
-  echo "Swapping to external display"
-  swap_to_external
 fi
