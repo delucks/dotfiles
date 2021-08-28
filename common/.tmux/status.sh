d8419de53beb59b28c5d03c1547ff3e703ba3628
delucks <ping@jamieluck.com>
2021-08-28 12:29:09 -0400

    Remove unused code

--- common/.tmux/status.sh
+++ common/.tmux/status.sh
@@ -4,14 +4,6 @@
 
 case "$(uname)" in
   Darwin*)
-    memcmd() {
-      PAGESZ=$(sysctl -n hw.pagesize)
-      FREE=$(( $(vm_stat | awk '/free/ {gsub(/\./, "", $3); print $3}') * PAGESZ))
-      TOTAL=$(sysctl -n hw.memsize)
-      RATIO=$(echo "$FREE/$TOTAL*100" | bc -l)
-      printf '%3.1f' "$RATIO"
-      echo %
-    }
     loadcmd() {
       uptime | rev | cut -d' ' -f '1-3' | rev
     }
@@ -20,9 +12,6 @@ case "$(uname)" in
     }
   ;;
   Linux*)
-    memcmd() {
-      free | awk '/Mem/{PERC=$NF/$2*100.0; printf "%3.1f%\n", PERC}'
-    }
     loadcmd() {
       cut -d' ' -f '1-3' /proc/loadavg
     }
@@ -31,15 +20,6 @@ case "$(uname)" in
     }
   ;;
   OpenBSD*)
-    memcmd() {
-      vmstat | tail -1 | while read -r _ _ act tot _; do
-        used="${act%%M}"
-        avail="${tot%%M}"
-        RATIO=$(echo "$used/$avail*100" | bc -l)
-        printf '%3.1f' "$RATIO"
-      done
-      echo %
-    }
     loadcmd() {
       sysctl -n vm.loadavg
     }
@@ -49,13 +29,6 @@ case "$(uname)" in
   ;;
   FreeBSD*)
     # freebsd
-    memcmd() {
-      avail=$(sysctl -n hw.physmem)
-      used=$(vmstat -H | tail -1 | awk '{print $5}')
-      RATIO=$(echo "$used/$avail*100" | bc -l)
-      printf '%3.1f' "$RATIO"
-      echo %
-    }
     loadcmd() {
       uptime | rev | cut -d' ' -f '1-3' | rev
     }
@@ -68,9 +41,6 @@ esac
 case $1 in
   load)
     loadcmd
-    ;;
-  memory|mem)
-    memcmd
     ;;
   cpu|cpucount)
     cpucmd
