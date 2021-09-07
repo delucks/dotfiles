ac5da4545ad4631375ce67afe09293dd944a94ba
delucks <ping@jamieluck.com>
2021-09-07 15:54:08 -0400

    spotifyctrl.sh: Control the "spt" interface using tmux rather than the desktop app via dbus

--- x11/bin/spotifyctrl.sh
+++ x11/bin/spotifyctrl.sh
@@ -1,27 +1,21 @@
 #!/usr/bin/env bash
-# a quick hack when I found out about the dbus interface
-TOGGLE="dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause"
-STOP="dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Stop"
-PREV="dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Previous"
-NEXT="dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Next"
-TRACK=$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' | grep -A 1 'xesam:title' | grep variant | sed 's/variant//g' | sed 's/string//g' | sed -e 's/^ *\"//g' -e 's/\" *$//g')
-ARTIST=$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' | grep -A 2 'xesam:artist' | grep string | grep -v xesam | sed 's/string//g' | sed -e 's/^ *\"//g' -e 's/\" *$//g')
+# This accompanies the "spotify" tmux session which launches the "spt" Spotify browser
+# We use the naming convention of that session to send keypresses to the "spt" client, controlling the spotifyd daemon
 
 case "$1" in
-	toggle)
-		$TOGGLE
-		;;
-	stop)
-		$STOP
-		;;
-	prev)
-		$PREV
-		;;
-	next)
-		$NEXT
-		;;
-	*)
+  toggle)
+    tmux send-keys -t '=spotify:=spt' 'Space'
+    ;;
+  stop)
+    systemctl --user stop spotifyd
+    ;;
+  prev)
+    tmux send-keys -t '=spotify:=spt' 'p'
+    ;;
+  next)
+    tmux send-keys -t '=spotify:=spt' 'n'
+    ;;
+  *)
     echo "Incorrect arg $1 passed to $0. Try one of {toggle,stop,prev,next}"
-		exit 1
+    exit 1
 esac
-hash dvol 2>/dev/null && dvol show
